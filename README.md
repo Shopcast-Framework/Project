6006ec0fae453352ed552d4dfae1e4dea2db733f
Thomas Chafiol <thomaschaf@gmail.com>
2015-09-12 15:20:42 +0200

    Add Express and make an Hello World

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # FullProject
+
+## Middend
+
+Run:
+
+node api/server.js
---
+++ api/server.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res) {
+    res.send('hello world');
+});
+
+app.listen(3000);
+console.log("Server running....");
