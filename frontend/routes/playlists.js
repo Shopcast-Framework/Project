c88eaff493e19e0bb68829affaa8c1899357bb03
Thomas Chafiol <thomaschaf@gmail.com>
2016-03-04 11:46:43 -0800

    Debug playlist.

--- frontend/routes/playlists.js
+++ frontend/routes/playlists.js
@@ -1,58 +1,47 @@
 'use strict';
 
-var express = require( 'express' );
-var router = express.Router();
-var Promise = require( 'promise' );
-var Rest = require('../rest');
-var menu    = require(__dirname + '/../menu.json');
+var express	= require( 'express' ),
+	router	= express.Router(),
+	Promise	= require( 'promise' ),
+	Rest	= require('../rest'),
+	menu	= require(__dirname + '/../menu.json');
 
 router.post('/', function(req, res) {
 
 	Rest.post('playlist', JSON.stringify(req.body)).then(function(response) {
 		console.log(response);
-		res.redirect('/playlists?message=' + response.message);
+		res.redirect('/playlists?message=' + response.body.message);
 	}, function(err) {
 		console.log(err);
-		res.redirect('/playlists?message=' + err.message);
+		res.redirect('/playlists?message=' + err.body.message);
 	});
 });
 
 router.get('/new', function(req, res) {
 
-	var promises = [];
-	var menu = null;
-
-	var promiseMenu = MenuModel.findAll({ where: { isActive: true }}).then(function(data) {
-		return JSON.parse(JSON.stringify(data));
-	});
-
-	promises.push(promiseMenu);
-
-	Promise.all(promises).then(function(values) {
-		res.render('playlists/new', {
-			title: 'Shopcast - Playlists',
-			titleContent: 'New playlist',
-			active: '/playlists',
-			menu: values[0]
-		});
-	}, function(err) {
-		console.log(err);
+	res.render('playlists/new', {
+		title: 'Shopcast - Playlists',
+		titleContent: 'New playlist',
+		active: '/playlists',
+		menu: menu
 	});
 
 });
 
-router.get('/', function( req, res ) {
+router.get('/', function(req, res) {
 
 	var promises = [];
 
 	promises.push(Rest.get('playlist'));
 
-	Promise.all( promises ).then(function(values) {
-		var playlists = values[1].body.playlists;
+	Promise.all(promises).then(function(values) {
+		var playlists = values[0].body.playlists;
+
 		res.render('playlists/index', {
 			title: 'Shopcast - Playlists',
 			titleContent: 'My playlists (' + playlists.length + ')',
 			active: '/playlists',
+			playlists: playlists,
 			menu: menu
 		});
 	}, function(err) {
