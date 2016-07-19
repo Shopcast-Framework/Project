bcdff84db919c41e78033ef6e8311c779fc2033a
Thomas Chafiol <thomaschaf@gmail.com>
2016-07-19 21:33:31 +0200

    Fix avatar on co facebook.

--- midend/modules/auth/strategy-facebook.js
+++ midend/modules/auth/strategy-facebook.js
@@ -24,7 +24,9 @@ var StrategyFacebook = function(app, passport, loginCallback) {
                 User
                 .create({
                     username: profile.displayName,
-                    facebookId: profile.userID
+                    name: profile.displayName,
+                    avatar: profile.photos[0].value,
+                    facebookId: profile.id
                 })
                 .then(function(user) {
                     return done(null, user);
