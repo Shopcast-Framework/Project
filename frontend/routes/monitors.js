c0cf015f48508b2d591ece171341c483e162ab5a
DESKTOP-9TCB9L8\guerin_f <florian.guerin@epitech.eu>
2016-11-22 23:55:07 +0100

    Add the sources of the monitor

--- frontend/routes/monitors.js
+++ frontend/routes/monitors.js
@@ -33,6 +33,18 @@ router.get('/delete/:id', middlewares.isLogged, function( req, res ) {
 
 });
 
+router.post('/:id',middlewares.isLogged, function(req, res) {
+
+	var id = req.params.id;
+
+	Rest.put('monitor/' + id, JSON.stringify(req.body)).then(function(response) {
+		res.redirect('/monitors/' + id + '?message=' + response.body.message);
+	}, function(err) {
+		console.log(err);
+		res.redirect('/monitors/' + id + '?message=' + response.body.message);
+	})
+});
+
 router.get('/:id', middlewares.isLogged, middlewares.language, function( req, res ) {
 
 	var promises = [];
