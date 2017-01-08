549ac61d3cbc4b79148cc74898d3adb529f1687c
Thomas Chafiol <thomaschaf@gmail.com>
2017-01-08 17:53:28 +0100

    Add file on the playlist show

--- applications_mobile/ios/Shopcast/Shopcast/File.swift
+++ applications_mobile/ios/Shopcast/Shopcast/File.swift
@@ -8,20 +8,17 @@
 
 import UIKit
 
-class Playlist {
-    var id: NSNumber
+class File {
     var name: String
     var description: String
     
-    init(_id: NSNumber, _name: String, _description: String) {
-        id = _id
+    init(_name: String, _description: String) {
         name = _name
         description = _description
     }
     
-    static func parse(_ blob: AnyObject) -> Playlist {
-        return Playlist(
-            _id: blob["id"] as! NSNumber,
+    static func parse(_ blob: AnyObject) -> File {
+        return File(
             _name: blob["name"] as! String,
             _description: blob["description"] as! String
         )
